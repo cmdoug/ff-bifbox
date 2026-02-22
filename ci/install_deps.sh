65033a42998896a7cc6d3ce2c76d977294782390
Chris Douglas <68232338+cmdoug@users.noreply.github.com>
2026-02-21 22:31:10 -0500

    Modify FreeFem installation path and add env variables
    Updated installation path for FreeFem and added environment variable exports.

--- ci/install_deps.sh
+++ ci/install_deps.sh
@@ -20,11 +20,16 @@ git clone https://github.com/cmdoug/FreeFem-sources
 cd FreeFem-sources
 git checkout develop
 autoreconf -i
-./configure --enable-download --enable-optim --with-petsc="${HOME}/petsc/real" --with-petsc_complex="${HOME}/petsc/complex" --prefix="${HOME}/freefem"
+./configure --enable-download --enable-optim --with-petsc="${HOME}/petsc/real" --with-petsc_complex="${HOME}/petsc/complex" --prefix="${HOME}/FreeFem-sources"
 ./3rdparty/getall -a
 ./reconfigure
 make -j 4
 make install
 cd -
+
+# Add FreeFEM to $PATH and export necessary environment variables
+export PATH=${PATH}:$HOME/FreeFem-sources/src/mpi:$HOME/FreeFem-sources/src/nw
+export FF_INCLUDEPATH="$HOME/FreeFem-sources/idp"
+export FF_LOADPATH="$HOME/FreeFem-sources/plugin/mpi;;$HOME/FreeFem-sources/plugin/seq"
 
 echo "=== Done: install_deps.sh ==="
