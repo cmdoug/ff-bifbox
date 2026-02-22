60f82e7e93471a412ffbed835d99a0723e7c4164
Chris Douglas <68232338+cmdoug@users.noreply.github.com>
2026-02-21 23:22:32 -0500

    Fix paths in complex PETSc configuration
    Updated configure script for complex architecture to use paths relative to the HOME directory.

--- ci/install_deps.sh
+++ ci/install_deps.sh
@@ -11,7 +11,7 @@ git clone https://gitlab.com/cmdoug/petsc
 cd petsc
 ./configure PETSC_ARCH=real --with-debugging=0 --with-scalar-type=real --download-mpich --dow--download-mumps --download-parmetis --download-metis --download-hypre --download-superlu --download-slepc --download-hpddm --download-ptscotch --download-suitesparse --download-scalapack --download-tetgen --download-mmg --download-parmmg --download-gsl=https://mirrors.kernel.org/gnu/gsl/gsl-2.8.tar.gz --download-bison=https://mirrors.kernel.org/gnu/bison/bison-3.8.2.tar.gz --download-slepc-configure-arguments="--download-arpack"
 make PETSC_ARCH=real
-./configure PETSC_ARCH=complex --with-debugging=0 --with-scalar-type=complex --with-mpich-dir=real --with-mumps-dir=real --with-parmetis-dir=real --with-metis-dir=real --with-superlu-dir=real --download-slepc --download-hpddm --download-htool --with-ptscotch-dir=real --with-suitesparse-dir=real --with-scalapack-dir=real --with-tetgen-dir=real --with-gsl-dir=real --with-bison-dir=real
+./configure PETSC_ARCH=complex --with-debugging=0 --with-scalar-type=complex --with-mpich-dir="${HOME}/petsc/real" --with-mumps-dir="${HOME}/petsc/real" --with-parmetis-dir="${HOME}/petsc/real" --with-metis-dir="${HOME}/petsc/real" --with-superlu-dir="${HOME}/petsc/real" --download-slepc --download-hpddm --download-htool --with-ptscotch-dir="${HOME}/petsc/real" --with-suitesparse-dir="${HOME}/petsc/real" --with-scalapack-dir="${HOME}/petsc/real" --with-tetgen-dir="${HOME}/petsc/real" --with-gsl-dir="${HOME}/petsc/real" --with-bison-dir="${HOME}/petsc/real"
 make PETSC_ARCH=complex
 cd -
 
